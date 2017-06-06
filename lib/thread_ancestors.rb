b1297531e9fc953443a7d8094534dc26d5c362ba
Adam Hart <ahart@zendesk.com>
2017-06-06 15:53:28 -0700

    Add base functionality

--- lib/thread_ancestors.rb
+++ lib/thread_ancestors.rb
@@ -1,2 +1,21 @@
 module ThreadAncestors
+  def initialize
+    @_parent = Thread.current
+    super
+  end
+
+  def ancestors
+    Enumerator.new do |enum|
+      t = Thread.current
+      while t = t.parent
+        enum.yield t
+      end
+    end.lazy
+  end
+
+  def parent
+    @_parent
+  end
 end
+
+Thread.prepend(ThreadAncestors)
